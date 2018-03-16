5b1d00b6e42ad4319cb096598e1e5ad5941be47c
Liam Cottrell <liamcottrell@hotmail.co.uk>
2018-03-16 13:36:14 +0000

    Beginins of the template
    Template is still being created, but this version should give a rough idea of how to add aspect to the page

--- app/index.js
+++ app/index.js
@@ -3,13 +3,15 @@ var app = express();
 
 app.set('view engine', 'ejs')
 
+app.use(express.static(__dirname + '/views'));
+
 app.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
 });
 
 app.get("/",function(req,res){
-	res.render('index');
+	res.render('partials/site');
 });
 
 app.listen(3000,function(){

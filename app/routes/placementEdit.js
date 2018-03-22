3417141f9099a4a7612a8034126a1b379c55c6bf
ANNA MCLEOD <1707029@rgu.ac.uk>
2018-03-22 18:58:10 +0000

    Added some comments to a couple of files

--- app/routes/placementEdit.js
+++ app/routes/placementEdit.js
@@ -1,5 +1,6 @@
 let router = require('express').Router();
 
+//Here is where the route will get the specific page from the Pages folder
 router.get('/', function(req, res, next) {
     res.render('pages/placementEdit');
 });

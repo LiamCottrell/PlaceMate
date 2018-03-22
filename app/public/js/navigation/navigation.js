e229745c9d7cf437be24db6323b4f54720256c01
Daramithes <33520316+Daramithes@users.noreply.github.com>
2018-03-22 21:10:10 +0000

    connected page to nav bar and used JS for button

--- app/public/js/navigation/navigation.js
+++ app/public/js/navigation/navigation.js
@@ -31,3 +31,10 @@ $("#searchButton").click(function() {
   pathname += "search";
   window.location.href = pathname;
 })
+
+$("#regButton").click(function() {
+  var pathname = window.location.pathname;
+  pathname = pathname.substring(0, pathname.indexOf("/"));
+  pathname += "register";
+  window.location.href = pathname;
+})

b9153483b6ea587716d4757938cea68768afcca3
Daramithes <33520316+Daramithes@users.noreply.github.com>
2018-03-22 00:27:32 +0000

    Jquery
    Added a ton of Jquery

--- app/server.js
+++ app/server.js
@@ -11,10 +11,15 @@ let searchResults = require('./routes/searchResults');
 
 let placementInfo = require('./routes/placementInfo');
 
+let login = require('./routes/login');
+
+let account = require('./routes/account');
+
 app.use('/', index);
 app.use('/search', searchResults);
 app.use('/placementInfo', placementInfo);
-
+app.use('/login', login);
+app.use('/account', account);
 app.use(function (req,res,next) {
   console.log("/" + req.method);
   next();

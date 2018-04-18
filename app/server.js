0ca1e7698eae0c1f65f05988e6adab5b2fdc662a
Liam Cottrell <liamcottrell@hotmail.co.uk>
2018-04-18 15:44:24 +0100

    Set Requirements / Code Tidy
    Declared requirments and tidyed up server.js to have more comments in relation to lecturer feedback.

--- app/server.js
+++ app/server.js
@@ -1,26 +1,33 @@
+/*Dependancies for our application*/
 var express = require("express");
-var app = express();
+var path = require('path');
+var bodyParser = require('body-parser'); // Allows us to Parse JSON
+var expressValidator = require('express-validator'); // Simple package to allow us to validate HTML forms
+var flash = require('flash'); // Create Flash Messages 
+var session = require('express-session'); // Use of Session to allow us to keep perpetual sessions between pages
+var passport = require('passport'); //  authentication middleware for Node.js
 
+/*Setup and configure Authentication System strategies*/
+var LocalStrategy = require('passport-local');
+var LocalStrategy = require('Strategy');
+
+/*Set application to be an express server, define where web content is in directory structure*/
+var app = express();
 app.use(express.static(__dirname + '/public'));
 
+/*Set Application vue engine to EJS*/
 app.set('view engine', 'ejs')
 
-//Here is where we link the pages through routes
+/*Here is where we link the pages through routes*/
 let index = require('./routes/index');
-
 let searchResults = require('./routes/searchResults');
-
 let placementInfo = require('./routes/placementInfo');
-
 let placementEdit = require('./routes/placementEdit');
-
 let login = require('./routes/login');
-
 let account = require('./routes/account');
-
 let register = require('./routes/register');
 
-//Here is where those links are then used when the user adds a key word after the / in the url
+/*Define page redirection paths*/
 
 app.use('/', index);
 app.use('/search', searchResults);
@@ -30,11 +37,13 @@ app.use('/login', login);
 app.use('/account', account);
 app.use('/register', register);
 
+/*Every Request to our page, display to console the request method for testing*/
 app.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
 });
 
+/*Set Port for our application to be listening to requests on*/
 app.listen(3000,function(){
   console.log("Live at Port 3000");
 });

e9b219e611610d57616ba5c34b26d8b1acdc5d40
Liam Cottrell <liamcottrell@hotmail.co.uk>
2018-04-18 16:23:06 +0100

    Fully Configued Server.js
    Fully disclosed server.js with all the correct dependancies set up and initialised to be used within our applicaiton.

--- app/server.js
+++ app/server.js
@@ -2,21 +2,39 @@
 var express = require("express");
 var path = require('path');
 var bodyParser = require('body-parser'); // Allows us to Parse JSON
-var expressValidator = require('express-validator'); // Simple package to allow us to validate HTML forms
-var flash = require('flash'); // Create Flash Messages 
+const { check, validationResult } = require('express-validator/check'); // Simple package to allow us to validate HTML forms
+const { matchedData, sanitize } = require('express-validator/filter'); // More Instruction: https://github.com/ctavan/express-validator
+var flash = require('connect-flash'); // Create Flash Messages 
 var session = require('express-session'); // Use of Session to allow us to keep perpetual sessions between pages
 var passport = require('passport'); //  authentication middleware for Node.js
 
 /*Setup and configure Authentication System strategies*/
-var LocalStrategy = require('passport-local');
-var LocalStrategy = require('Strategy');
+var LocalStrategy = require('passport-local').Strategy;
 
-/*Set application to be an express server, define where web content is in directory structure*/
+/*Set application to be an express server, define where web assets are in directory structure*/
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
 /*Set Application vue engine to EJS*/
 app.set('view engine', 'ejs')
+
+/*Declare BodyParser Middleware*/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/*Declare Express session*/
+app.use(session({
+	secret: 'JohnIsaacs',
+	saveUninitialized: true,
+	resave: true
+}));
+
+/*Passport Initialisation*/
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*Declare flash*/
+app.use(flash());
 
 /*Here is where we link the pages through routes*/
 let index = require('./routes/index');
@@ -28,7 +46,6 @@ let account = require('./routes/account');
 let register = require('./routes/register');
 
 /*Define page redirection paths*/
-
 app.use('/', index);
 app.use('/search', searchResults);
 app.use('/placementInfo', placementInfo);
@@ -37,13 +54,19 @@ app.use('/login', login);
 app.use('/account', account);
 app.use('/register', register);
 
-/*Every Request to our page, display to console the request method for testing*/
+/*Every Request to our page*/
 app.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
+	/*Display to console the request method for testing*/
+  	console.log("/" + req.method);
+	/*Global Variables*/
+  	res.locals.success_msg = req.flash('success_msg';)
+  	res.locals.error_msg = req.flash('error_msg');
+  	/*Passport specific error messages*/
+  	res.locals.error = req.flash('error');
+  	next();
 });
 
 /*Set Port for our application to be listening to requests on*/
 app.listen(3000,function(){
-  console.log("Live at Port 3000");
+	console.log("Live at Port 3000");
 });

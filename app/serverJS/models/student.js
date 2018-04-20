65e9bf7f46b8ee4303e36cf157d0bdff094d22ec
Liam Cottrell <liamcottrell@hotmail.co.uk>
2018-04-20 09:44:46 +0100

    Code Commenting for student creation
    Code commented for purpose and functionality

--- app/serverJS/models/student.js
+++ app/serverJS/models/student.js
@@ -1,9 +1,10 @@
+/*Purpose of this file is to specify the data the the Student collection will be storing*/
+
+/*Declare necissary dependancies*/
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-/*const Schema = mongoose.Schema;*/
-
-//Create Schema and Model
+/*Create Schema using mongoose and apply schema as a template of how our data will be stored*/
 
 const StudentSchema = mongoose.Schema({
   first_name: {
@@ -29,8 +30,10 @@ const StudentSchema = mongoose.Schema({
 
 var Student = module.exports = mongoose.model('Students', StudentSchema);
 
-/*module.exports = Student*/
 
+/*Export function called "CreateStudent"
+This will allow us to pass in an object with student information
+It will then hash the password, then save the user to the database.*/
 module.exports.createStudent = function(newStudent, callback){
 	bcrypt.genSalt(10, function(err, salt) {
 		bcrypt.hash(newStudent.password, salt, function(err, hash){

fe4b68f05ce247375ed230caf9c008247a381db5
Liam Cottrell <liamcottrell@hotmail.co.uk>
2018-04-21 15:52:59 +0100

    Created Search page
    Search page now collects information from placementController and implements it into the interface

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,7 +1,23 @@
-let router = require('express').Router();
+/*
+    Author: Liam Cottrell,
+    Purpose: Handle the retrieval of Placement information, using our Placement Controller,
+    to populate the page with search results.
+*/
 
+/*Dependencies*/
+let router = require('express').Router();
+let PlacementController = require('../serverJS/controllers/placement.js');
+
+/*On load of search index*/
 router.get('/', function(req, res, next) {
-    res.render('pages/search');
+    /*Query the Placement controller and search for all relivent placements with
+    the "GET" paramiter of SearcgParameter*/
+    PlacementController.SearchPlacements(req.query.SearchPlacements).then(function(results){
+        /*Send the result of the query to the search page to be handled*/
+        res.render('pages/search', {
+          placement: results
+        });
+    })
 });
 
 module.exports = router;
